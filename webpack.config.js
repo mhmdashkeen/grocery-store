cf165feb6f1439682bbf6354db1b6095d96c6794
Ashkeen <ashkeen.a.m@gmail.com>
2024-01-30 18:43:00 +0530

    config files

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  target: 'web',
+  devServer: {
+    port: '3000',
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        use: 'babel-loader', 
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "scoped-css-loader", "postcss-loader"],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html'
+    })
+  ]
+};
